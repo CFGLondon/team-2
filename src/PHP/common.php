e6662de4769f88360cbdae36eb8aa43e64226b66
ANuclearError <aidan@aidanogrady.com>
2015-12-05 01:32:41 +0000

    Moved login server side stuff to its own file.

--- src/PHP/common.php
+++ src/PHP/common.php
@@ -11,17 +11,4 @@ function executeSELECT($query) {
 		return $result;
 	}
 }
-
-function login($username, $password) {
-	$query_get_user = "SELECT password FROM user where username='$username'";
-	$result = executeSELECT($query_get_user);
-	
-	if (strcmp($password, $result->fetch_assoc()["password"]) == 0) {
-		echo "password matched";
-	} else {
-		echo "login failed";
-	}
-}
-
-login ("testuser", "testpass");
 ?>
--- src/login.php
+++ src/login.php
@@ -20,16 +20,16 @@
         <div class="container">
             <h1 class="header center purple-text lighten-1">Welcome Back!</h1>
             <div class="row">
-                <form class="col s12">
+                <form class="col s12" method="post" action="php/common.php">
                     <div class="row">
                         <div class="input-field col s12">
-                            <input id="email" type="email" class="validate">
+                            <input id="email" name="email" type="email" class="validate">
                             <label for="email">Email</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
-                            <input id="password" type="password" class="validate">
+                            <input id="password" name="password" type="password" class="validate">
                             <label for="password">Password</label>
                         </div>
                     </div>
