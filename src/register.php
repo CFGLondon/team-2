db1a1f971f64bd2a1fdb2fd7d37ceb4e2eaff710
ANuclearError <aidan@aidanogrady.com>
2015-12-05 02:39:33 +0000

    Updated register.php.

--- src/register.php
+++ src/register.php
@@ -8,95 +8,95 @@
 </head>
 <body>
 <header>
-  <nav class="light-blue" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Age Connect</a>
-      <?php include "php/include/nav.php"?>
-    </div>
-  </nav>
+    <nav class="light-blue" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="#"
+                                              class="brand-logo">Age Connect</a>
+            <?php include "php/include/nav.php" ?>
+        </div>
+    </nav>
 </header>
 
 <div class="container">
-  <div class="row">
-    <form class="col s12">
-	
+    <div class="row">
+        <form class="col s12">
+            <div class="section">
+                <h5>Thank you for your interest</h5>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input name="email" type="email" class="validate">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input name="password" type="password" class="validate">
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6 offset-s6">
+                        <input name="confirmpassword" type="password" class="validate">
+                        <label for="confirmpassword">Confirm Password</label>
+                    </div>
 
-  <div class="section">
-    <h5>Personal Details</h5>
-      <div class="row">
-        <div class="input-field col s6">
-          <input name="first_name" type="text" class="validate">
-          <label for="first_name">First Name</label>
-        </div>
-        <div class="input-field col s6">
-          <input name="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="telephone" type="email" class="validate">
-          <label for="telephone">Telephone</label>
-        </div>
-      </div>
-	 <div class="input-field col s12">
-		<input name="dob" type="date" class="datepicker">
-        <label for="dob">Date of birth</label>
-	 </div>
-	 </div> 
-	 
-  <div class="divider"></div>
-  <div class="section">
-    <h5>Address</h5>
-	
-	<div class="row">
-        <div class="input-field col s4">
-          <input name="houseno" type="text" class="validate">
-          <label for="houseno">House No.</label>
-        </div>
-        <div class="input-field col s8">
-          <input name="street" type="text" class="validate">
-          <label for="street">Street</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="address1" type="email" class="validate">
-          <label for="address1">Address line 1</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="address2" type="email" class="validate">
-          <label for="address2">Address line 2</label>
-        </div>
-      </div>
-	  <div class="row">
-        <div class="input-field col s8">
-          <input name="town" type="text" class="validate">
-          <label for="town">Town</label>
-        </div>
-        <div class="input-field col s4">
-          <input name="postcode" type="text" class="validate">
-          <label for="postcode">Postcode</label>
-        </div>
-      </div>
-  </div>
-
-	  
-	  <div class="row">
-			<button class="btn waves-effect waves-light" type="submit" name="action">Submit
-				<i class="material-icons right">send</i>
-			</button>
-	  </div>
-    </form>
-  </div>
-
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="section">
+                <h5>Personal Details</h5>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input name="first_name" type="text" class="validate">
+                        <label for="first_name">First Name</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input name="last_name" type="text" class="validate">
+                        <label for="last_name">Last Name</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input name="telephone" type="email" class="validate">
+                        <label for="telephone">Telephone</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input name="dob" type="date" class="datepicker">
+                        <label for="dob">Date of birth</label>
+                    </div>
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="section">
+                <h5>Address</h5>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input name="address1" type="text" class="validate">
+                        <label for="address1">Address line 1</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input name="address2" type="text" class="validate">
+                        <label for="address2">Address line 2</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s8">
+                        <input name="town" type="text" class="validate">
+                        <label for="town">Town</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <input name="postcode" type="text" class="validate">
+                        <label for="postcode">Postcode</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <button class="btn waves-effect waves-light" type="submit"
+                        name="action">Submit
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </form>
+    </div>
 </div>
 
 <!-- Compiled and minified JavaScript -->
