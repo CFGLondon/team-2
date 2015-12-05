b18d16b1b6a5e7d4a8d389803b9edaac697f6992
JonasH21 <szjonas4@gmail.com>
2015-12-05 04:04:46 +0000

    Added content to index page

--- src/index.php
+++ src/index.php
@@ -15,6 +15,70 @@
     </nav>
 </header>
 
+<body>
+    <div class="container">
+      <br><br>
+      <h1 class="header center purple-text lighten-1">Age Connect</h1>
+      <div class="row">
+        <h6 class="header col s12 bold">What is Age Connect?</h6>
+		<br><p>No one should be alone at Christmas, yet thousands of elderly people are still alone. Age UK hopes to change that this Christmas,
+		with our new initiative, Age Connect.
+		Age Connect is our national telephone friendship service for people in later life. Our team of volunteers provide a valuable and rewarding
+		service to thousands of elderly people across the UK, by giving people who need it most a friend whom they can talk to each week. </p>
+	  </div>
+	  
+	  
+<div class="row">
+        <div class="col s12 m7">
+          <div class="card">
+            <div class="card-image">
+              <img src="https://cdn.ageuk.org.uk/Global/no%20one%20should%20have%20no%20one/550x180_No-one_xmas_Home-page.jpg" alt="#NoOne should have no one at Christmas" width="550" height="180">
+              <!--<span class="card-title">Card Title</span>-->
+            </div>
+            <div class="card-content">
+              <p>No one should have no one at Christmas and yet this year many older people will have no one to share it with. 
+			  Will you help us answer a call from a lonely older person and give them much-needed support?</p>
+            </div>
+            <div class="card-action">
+              <a href="register.php">Call someone this Christmas</a>
+            </div>
+          </div>
+        </div>
+      </div>	  
+
+	  <h5 class="header col s12 bold">Age Connect and You: change someone's life for the better</h5>
+      <br><div class="row" vertical-align="center">
+		<div class="col s6">
+			<h6 class="header col s12 bold">What does it involve?</h6>
+			<br><p>We are recruiting for new volunteers to make weekly calls to support and befriend people who may be socially isolated. 
+			As a volunteer, you will be committed to a weekly half-hour call. This is a rewarding long-term commitment. 
+			Nearly 90% of our volunteers report intending to continue volunteering over the next 12 months.
+			If you can't make a call on any particular week, that's no problem! Our team of full-time workers are always here for support and to ensure your match receives a weekly call.</p>
+		</div>
+		<div class="col s6">
+			<h6 class="header col s12 bold">What's in it for you?</h6>
+			<br><p>Once you start your Age Connect journey with us, we’ll train you, support you and then match you with an older person who’s waiting for your call. 
+			Each week, you’ll enjoy a phone chat, learn about the person you’re calling and have our support every step of the way. 
+			Many of our volunteers &#8212; over 85% report a sense of happiness and satisfaction from volunteering on this programme. 
+			Full support will be offered to our volunteers, with induction and training programmes, regular communication to keep you updated,
+			and advice from a named member of staff who will listen to any feedback or concerns you may have.</p>
+		</div></div>
+		
+		<br><div class="row">
+		<div class="col s12" vertical-align="center">
+			<h6 class="header col s12 bold">Call someone this Christmas</h6>
+			<br><p>Thankyou for reading this far! So Age Connect sounds like something you would like to volunteer for? 
+			To sign up, we have a short sign-up form, and then while we validate any necessary legal checks, you will have access to 
+			our online training portal. You will also be able to change your profile, which we use to match you to someone who you'll likely get on with!
+			As soon as your application is validated you will be able to find a match with an elderly person waiting for your call.</p>
+		</div>
+		</div>
+      <br><br>
+    <a class="waves-effect waves-light btn purple lighten-1" id="applyNow" href="register.php">Apply Now</a>
+    </div>
+<br><br>
+</body>
+
 <!-- Compiled and minified JavaScript -->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="js/materialize.min.js"></script>
