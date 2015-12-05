368dd14683933e609b74f086a0da65b25024ef56
Grant <togglewhenready@gmail.com>
2015-12-05 03:13:44 +0000

    Re-added php/login.php

---
+++ src/php/login.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'connect.php';
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$query_get_user = "SELECT password FROM user where email='$email'";
+$result = executeSELECT($query_get_user);
+
+if (strcmp($password, $result->fetch_assoc()["password"]) == 0) {
+	echo "password matched";
+	} else {
+	echo "login failed";
+	}
+
+?>
